189832c051b5b725ac8702d6e1ffa5aaa46f1609
Fantomas42 <fantomas42@gmail.com>
2010-02-19 16:47:13 +0100

    adding project files

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='emencia.django.ged',
+      version=version,
+      description="Very simple DMS application for Django",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      keywords='django, documents, management, dms',
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'License :: OSI Approved :: BSD License',
+          ],
+
+      author='Fantomas42',
+      author_email='fantomas42@gmail.com',
+      url='http://emencia.fr',
+      
+      license='BSD License',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['emencia', 'emencia.django'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
