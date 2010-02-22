29a805427a00f01129aac2c207e1a948252f0de2
Fantomas42 <fantomas42@gmail.com>
2010-02-22 14:47:09 +0100

    std setup

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ version = '0.1'
 
 setup(name='emencia.django.ged',
       version=version,
-      description="Very simple DMS application for Django",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      description='Very simple DMS application for Django',
+      long_description=open('README.rst').read() + '\n' +
+                       open(os.path.join('docs', 'HISTORY.txt')).read(),
       keywords='django, documents, management, dms',
       classifiers=[
           'Intended Audience :: Developers',
